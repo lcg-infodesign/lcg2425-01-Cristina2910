50b1a5b99fda4b97c6ed7c8fbb57465cf56e95bd
Cristina2910 <cristina.pedrazzini@mail.polimi.it>
2024-10-23 13:43:11 +0200

    assignment 1 - Vera Molnar

--- sketch.js
+++ sketch.js
@@ -1,17 +1,54 @@
-function preload() {
-  // put preload code here
-}
+let cols; //numero di colonne
+let rows; //numero di righe
+let cellSize;
+let margin = 20; 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  //le dimensioni del canvas dipendono da quelle della finestra
+  createCanvas(windowWidth, windowHeight); 
+  cols = floor((width - 2 * margin) / 20); //calcola le colonne considerando il margine
+  rows = floor((height - 2 * margin) / 20); //calcola le righe considerando il margine
+  cellSize = (width - 2 * margin) / cols; //regola la dimensione delle celle
+  strokeWeight(4); //spessore delle linee
+  noLoop(); //disegna solo una volta
 }
 
 function draw() {
-  // put drawing code here
+  background("white"); 
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * cellSize + margin; //aggiungi il margine all'asse X
+      let y = j * cellSize + margin; //aggiungi il margine all'asse Y
+      drawRandomLine(x, y, cellSize); //disegna le linee casuali
+    }
+  }
+}
+
+//funzione per disegnare linee casuali in una cella
+function drawRandomLine(x, y, size) {
+  let choice = int(random(4)); //serve a scegliere casualmente una delle 4 opzioni
+  
+  stroke(0); //riferito alle linee nere
+  if (choice === 0) {
+    //linea orizzontale
+    line(x, y + size / 2, x + size, y + size / 2);
+  } else if (choice === 1) {
+    // inea verticale
+    line(x + size / 2, y, x + size / 2, y + size);
+  } else if (choice === 2) {
+    //linea diagonale che va dall'angolo in alto sinistra a quello in basso destra
+    line(x, y, x + size, y + size);
+  } else if (choice === 3) {
+    //linea diagonale che va dall'angolo in basso sinistra a quello in alto destra
+    line(x, y + size, x + size, y);
+  }
+}
+
+//voglio ridimensionare il canvas quando la finestra cambia dimensione
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  cols = floor((width - 2 * margin) / 20); //per ricalcolare le colonne considerando il margine
+  rows = floor((height - 2 * margin) / 20); //per ricalcolare le righe considerando il margine
+  cellSize = (width - 2 * margin) / cols; //per ricalcolare la dimensione delle celle
+  redraw(); 
 }
